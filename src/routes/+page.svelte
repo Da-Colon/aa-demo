ffe8ba0e526f37ab523c8fd613e3b572c2bfaa64
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-07-11 14:09:12 -0400

    style cleanup

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,9 +8,14 @@
 </svelte:head>
 
 <section>
-
+	
 </section>
 
 <style>
-	
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
 </style>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,58 +1,61 @@
 <script>
-  let logoText = 'Mako Wallet';
+	let logoText = 'Mako Wallet';
 </script>
 
 <header>
-  <div class="logo" data-text={logoText}>
-    {logoText}
-  </div>
+	<div class="logo" data-text={logoText}>
+		{logoText}
+	</div>
 </header>
 
 <style>
-  @keyframes flame {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
+	@keyframes flame {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
 
-  header {
-    display: flex;
-    justify-content: center;
-    background: var(--color-black);
-    padding: 20px;
-  }
+	header {
+		display: flex;
+		justify-content: center;
+		padding: 20px;
+	}
 
-  .logo {
-    font-size: 4rem;
-    font-family: 'Lakki Reddy', sans-serif;
-    text-align: center;
-    color: var(--color-white);
+	.logo {
+		border-radius: 10px;
+		color: var(--color-white);
+		font-size: 4rem;
+		font-family: 'Lakki Reddy', sans-serif;
 		letter-spacing: 0.5rem;
-    background: var(--color-black);
-    text-shadow: 0px 1px 10px var(--color-wallet-green);
-    border-radius: 10px;
-    overflow: hidden;
-    position: relative;
-  }
+		overflow: hidden;
+		position: relative;
+		text-align: center;
+		text-shadow: 0px 1px 10px var(--color-wallet-green);
+	}
 
-  .logo::after {
-    content: attr(data-text);
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    color: transparent;
-    background: linear-gradient(90deg, var(--color-wallet-green), var(--color-wallet-green-dark), var(--color-wallet-green));
-    background-size: 200% 200%;
-    -webkit-background-clip: text;
-    background-clip: text;
-    animation: flame 2s linear infinite;
-  }
+	.logo::after {
+		content: attr(data-text);
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		color: transparent;
+		background: linear-gradient(
+			90deg,
+			var(--color-wallet-green),
+			var(--color-wallet-green-dark),
+			var(--color-wallet-green)
+		);
+		background-size: 200% 200%;
+		-webkit-background-clip: text;
+		background-clip: text;
+		animation: flame 2s linear infinite;
+	}
 </style>
