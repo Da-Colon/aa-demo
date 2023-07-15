e4aa3c00ee55322a4e663ca3c9aefeb7928cea33
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-07-15 00:59:49 -0400

    saving some changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,17 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
-	import { BASE_ENTRYPOINT_ADDRESS, web3 } from '$lib/stores/web3';
+	import { BASE_ENTRYPOINT_ADDRESS, GAS_POLICY_ID, SIMPLE_ACCOUNT_FACTORY_ADDRESS, web3 } from '$lib/stores/web3';
+	import { DemoNFT__factory, SmartAccountFactory__factory } from '../typechain';
 	import type { Web3Store } from '$lib/types';
 	import { alchemyPaymasterAndDataMiddleware } from '@alchemy/aa-alchemy';
 	let state: Web3Store;
-
+	const NFT_ADDRESS = '0x0962C095fF4af9d35D84918a4DD0711Fc8362Ff6';
+	const TOKEN_URI = {
+		name: 'Mako Energy',
+		description:
+			'Mako Energy is a decentralized energy company that is building a new energy economy in Midgar.',
+		image: 'https://pin.ski/3NKU3VC'
+	};
 	web3.subscribe((value) => {
 		state = value;
 	});
@@ -14,33 +21,48 @@
 		// mint nft
 	}
 	// mint nft with paymaster
-	function mintNFTWithERC20GasPayment() {
-		if (!state.signer || !state.provider || !state.alchemy || !state.adapter)
+	async function mintNFTWithGasSponsor() {
+		if (!state.accountSigner || !state.provider || !state.provider) {
 			return console.log('no signer');
-		const client = state.signer.getPublicErc4337Client();
-		const withPaymaster = state.signer.withPaymasterMiddleware(
+		}
+
+		const withPaymaster = state.accountSigner.withPaymasterMiddleware(
 			alchemyPaymasterAndDataMiddleware({
-				provider: client,
-				policyId: '',
+				provider: state.provider.rpcClient,
+				policyId: GAS_POLICY_ID,
 				entryPoint: BASE_ENTRYPOINT_ADDRESS
 			})
 		);
 		// mint nft
+		const smartAddress = await withPaymaster.getAddress();
+		const target = NFT_ADDRESS;
+		const txData = DemoNFT__factory.createInterface().encodeFunctionData('mintEnergy', [
+			target,
+			JSON.stringify(TOKEN_URI)
+		]);
+		const tx = await withPaymaster.sendUserOperation({
+			target: smartAddress as `0x${string}`,
+			data: txData as `0x${string}`,
+			value: 0n
+		});
+		console.log('🚀 ~ file: +page.svelte:48 ~ tx:', tx);
 	}
-	
+
 	// mint nft with gas sponsor
-	function mintNFTWithGasSponsor() {
-		if (!state.signer || !state.provider || !state.alchemy || !state.adapter)
+	async function mintNFTWithERC20GasPayment() {
+		if (!state.accountSigner || !state.provider || !state.provider)
 			return console.log('no signer');
-		const client = state.signer.getPublicErc4337Client();
-		const withPaymaster = state.signer.withPaymasterMiddleware(
-			alchemyPaymasterAndDataMiddleware({
-				provider: client,
-				policyId: '',
-				entryPoint: BASE_ENTRYPOINT_ADDRESS
-			})
-		);
-		// mint nft
+		const smartAddress = await state.accountSigner.getAddress();
+		const target = NFT_ADDRESS;
+		const txData = DemoNFT__factory.createInterface().encodeFunctionData('mintEnergy', [
+			target,
+			JSON.stringify(TOKEN_URI)
+		]);
+		await state.accountSigner.sendUserOperation({
+			target: smartAddress as `0x${string}`,
+			data: txData as `0x${string}`,
+			value: 0n
+		});
 	}
 </script>
 
@@ -52,17 +74,17 @@
 <section>
 	<Container>
 		<section>
-			Mint NFT
-			<button class="button-primary">Pay With Paymaster (USDC)</button>
+			<button class="button-primary">Mint NFT With Paymaster (USDC)</button>
 		</section>
 	</Container>
 	<Container>
 		<section>
-			Mint NFT
-			<button class="button-primary">Pay With Gas Sponsor</button>
+			<button class="button-primary" on:click={mintNFTWithGasSponsor}>Mint NFT With Gas Sponsor</button>
 		</section>
 	</Container>
-	<Container>Sign up for Subscriptions (tbd)</Container>
+	<Container>
+		<button class="button-primary" disabled>Sign up for Subscriptions (tbd)</button>
+	</Container>
 </section>
 
 <style>
