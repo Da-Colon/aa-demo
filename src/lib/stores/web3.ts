5e15164b0ee547dbedac3d5f69f87e8607852644
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-07-15 01:15:44 -0400

    connects smart account!!!!

--- src/lib/stores/web3.ts
+++ src/lib/stores/web3.ts
@@ -7,10 +7,13 @@ import { Alchemy, Network, type AlchemyProvider } from 'alchemy-sdk';
 
 // Alchemy API key for the application
 const ALCHEMY_API_KEY = import.meta.env.VITE_ALCHEMY_API_KEY;
+export const GAS_POLICY_ID = import.meta.env.VITE_GAS_POLICY_ID;
 // https://github.com/eth-infinitism/account-abstraction/blob/develop/contracts/samples/SimpleAccount.sol
-export const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454";
+// export const SIMPLE_ACCOUNT_FACTORY_ADDRESS = "0x9406Cc6185a346906296840746125a0E44976454";
+// my version
+export const SIMPLE_ACCOUNT_FACTORY_ADDRESS = '0x1994d131578F9c652bdFC0b623e93d03c7022940';
 // https://github.com/eth-infinitism/account-abstraction/blob/develop/contracts/core/EntryPoint.sol
-export const BASE_ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
+export const BASE_ENTRYPOINT_ADDRESS = '0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789';
 
 /**
  * Creates an account signer and obtains the network details.
@@ -19,28 +22,32 @@ export const BASE_ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d27
  * @returns The account signer and network details
  */
 const createAccountSigner = async (signer: Signer, alchemyProvider: AlchemyProvider) => {
-  // Obtain the current network from the Alchemy provider
-  const network = await alchemyProvider.getNetwork();
+	// Obtain the current network from the Alchemy provider
+	const network = await alchemyProvider.getNetwork();
 
-  // Create an adapter from the Ethers provider
-  const adapter = EthersProviderAdapter.fromEthersProvider(
-    alchemyProvider,
-    BASE_ENTRYPOINT_ADDRESS
-  );
+	// Create an adapter from the Ethers provider
+	const adapter = EthersProviderAdapter.fromEthersProvider(
+		alchemyProvider,
+		BASE_ENTRYPOINT_ADDRESS
+	);
 
-
-  // Connect the adapter to the account to create the account signer
-  const accountSigner = adapter.connectToAccount(
-    (rpcClient) =>
-      new SimpleSmartContractAccount({
-        entryPointAddress: BASE_ENTRYPOINT_ADDRESS,
-        chain: getChain(network.chainId),
-        owner: convertWalletToAccountSigner({ signMessage: signer.signMessage, getAddress: signer.getAddress } as Wallet),
-        factoryAddress: SIMPLE_ACCOUNT_FACTORY_ADDRESS,
-        rpcClient,
-      })
-  );
-  return { accountSigner, network, adapter };
+	// Connect the adapter to the account to create the account signer
+	const accountSigner = adapter.connectToAccount(
+		(rpcClient) =>
+			new SimpleSmartContractAccount({
+				entryPointAddress: BASE_ENTRYPOINT_ADDRESS,
+				chain: getChain(network.chainId),
+				owner: {
+					signMessage: async (msg: Uint8Array) => (await signer.signMessage(msg)) as `0x${string}`,
+					getAddress: async () => (await signer.getAddress()) as `0x${string}`
+				},
+				factoryAddress: SIMPLE_ACCOUNT_FACTORY_ADDRESS,
+				rpcClient
+			})
+	);
+	const address = await accountSigner.getAddress();
+	console.log('🚀 ~ file: web3.ts:47 ~ address:', address);
+	return { accountSigner, network, adapter };
 };
 
 /**
@@ -48,88 +55,95 @@ const createAccountSigner = async (signer: Signer, alchemyProvider: AlchemyProvi
  * @returns An object that contains the web3 store and its related functions
  */
 function createWeb3Store() {
-  // Create a writable web3 store
-  const { subscribe, set, update } = writable<Web3Store>({
-    isConnected: false,
-    provider: null,
-    signer: null,
-    address: null,
-    network: null,
-    alchemy: null,
-    adapter: null,
-  });
+	// Create a writable web3 store
+	const { subscribe, set, update } = writable<Web3Store>({
+		isConnected: false,
+		provider: null,
+		signer: null,
+		address: null,
+		network: null,
+		alchemy: null,
+		adapter: null
+	});
 
-  return {
-    subscribe,
-    connect: async () => {
-      // Check if Ethereum is defined in the window object
-      if (typeof window.ethereum !== 'undefined') {
-        // Create a new Ethers provider
-        const provider = new ethers.providers.Web3Provider(window.ethereum);
+	return {
+		subscribe,
+		connect: async () => {
+			// Check if Ethereum is defined in the window object
+			if (typeof window.ethereum !== 'undefined') {
+				// Create a new Ethers provider
+				const provider = new ethers.providers.Web3Provider(window.ethereum);
 
-        // Create a new Alchemy instance and obtain the provider
-        const alchemy = new Alchemy({
-          apiKey: ALCHEMY_API_KEY,
-          network: Network.ETH_GOERLI,
-        });
-        const alchemyProvider = await alchemy.config.getProvider();
+				// Create a new Alchemy instance and obtain the provider
+				const alchemy = new Alchemy({
+					apiKey: ALCHEMY_API_KEY,
+					network: Network.ETH_GOERLI
+				});
+				const alchemyProvider = await alchemy.config.getProvider();
 
+				// Request access to user accounts
+				const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
 
-        // Request access to user accounts
-        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' });
+				// If there are accounts available
+				if (accounts && accounts.length > 0) {
+					// Get the signer from the provider
+					const signer = provider.getSigner();
 
-        // If there are accounts available
-        if (accounts && accounts.length > 0) {
-          // Get the signer from the provider
-          const signer = provider.getSigner();
+					// Create the account signer and obtain the network
+					const { accountSigner, network, adapter } = await createAccountSigner(
+						signer,
+						alchemyProvider
+					);
 
-          // Create the account signer and obtain the network
-          const { accountSigner, network, adapter } = await createAccountSigner(signer, alchemyProvider);
-  
-          // Obtain the address from the signer
-          const address = await signer.getAddress();
+					// Obtain the address from the signer
+					const address = await signer.getAddress();
 
-          // Update the web3 store with the new details
-          set({
-            isConnected: true,
-            provider: alchemyProvider,
-            signer: accountSigner,
-            address: address,
-            network: network,  // Set the network field
-            alchemy: alchemy,  // Set the alchemy field
-            adapter: adapter,  // Set the adapter field
-          });
+					// Update the web3 store with the new details
+					set({
+						isConnected: true,
+						provider: alchemyProvider,
+						signer: accountSigner,
+						address: address,
+						network: network, // Set the network field
+						alchemy: alchemy, // Set the alchemy field
+						adapter: adapter // Set the adapter field
+					});
 
-          // Update the address in the web3 store when the accounts change
-          window.ethereum.on('accountsChanged', (accounts: string[]) => {
-            update(state => ({ ...state, address: accounts[0] }));
-          });
+					// Update the address in the web3 store when the accounts change
+					window.ethereum.on('accountsChanged', (accounts: string[]) => {
+						update((state) => ({ ...state, address: accounts[0] }));
+					});
 
-          // Update the provider, signer, and network in the web3 store when the chain changes
-          window.ethereum.on('chainChanged', async () => {
-            const { accountSigner, network } = await createAccountSigner(signer, alchemyProvider);
-            update(state => ({ ...state, provider: alchemyProvider, signer: accountSigner, network: network }));
-          });
-        } else {
-          console.error("Unable to get accounts. User denied permission?");
-        }
-      } else {
-        console.error("Please install MetaMask!");
-      }
-    },
-    disconnect: () => {
-      // Reset the web3 store when disconnecting
-      set({
-        isConnected: false,
-        provider: null,
-        signer: null,
-        address: null,
-        network: null,
-        alchemy: null,
-        adapter: null,
-      });
-    },
-  };
+					// Update the provider, signer, and network in the web3 store when the chain changes
+					window.ethereum.on('chainChanged', async () => {
+						const { accountSigner, network } = await createAccountSigner(signer, alchemyProvider);
+						update((state) => ({
+							...state,
+							provider: alchemyProvider,
+							signer: accountSigner,
+							network: network
+						}));
+					});
+				} else {
+					console.error('Unable to get accounts. User denied permission?');
+				}
+			} else {
+				console.error('Please install MetaMask!');
+			}
+		},
+		disconnect: () => {
+			// Reset the web3 store when disconnecting
+			set({
+				isConnected: false,
+				provider: null,
+				signer: null,
+				address: null,
+				network: null,
+				alchemy: null,
+				adapter: null
+			});
+		}
+	};
 }
 
 // Export the created web3 store
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,11 @@
 <script lang="ts">
 	import Container from '$lib/components/Container.svelte';
-	import { BASE_ENTRYPOINT_ADDRESS, GAS_POLICY_ID, SIMPLE_ACCOUNT_FACTORY_ADDRESS, web3 } from '$lib/stores/web3';
+	import {
+		BASE_ENTRYPOINT_ADDRESS,
+		GAS_POLICY_ID,
+		SIMPLE_ACCOUNT_FACTORY_ADDRESS,
+		web3
+	} from '$lib/stores/web3';
 	import { DemoNFT__factory, SmartAccountFactory__factory } from '../typechain';
 	import type { Web3Store } from '$lib/types';
 	import { alchemyPaymasterAndDataMiddleware } from '@alchemy/aa-alchemy';
@@ -22,43 +27,42 @@
 	}
 	// mint nft with paymaster
 	async function mintNFTWithGasSponsor() {
-		if (!state.accountSigner || !state.provider || !state.provider) {
+		if (!state.signer || !state.provider || !state.provider) {
 			return console.log('no signer');
 		}
 
-		const withPaymaster = state.accountSigner.withPaymasterMiddleware(
-			alchemyPaymasterAndDataMiddleware({
-				provider: state.provider.rpcClient,
-				policyId: GAS_POLICY_ID,
-				entryPoint: BASE_ENTRYPOINT_ADDRESS
-			})
-		);
-		// mint nft
-		const smartAddress = await withPaymaster.getAddress();
-		const target = NFT_ADDRESS;
-		const txData = DemoNFT__factory.createInterface().encodeFunctionData('mintEnergy', [
-			target,
-			JSON.stringify(TOKEN_URI)
-		]);
-		const tx = await withPaymaster.sendUserOperation({
-			target: smartAddress as `0x${string}`,
-			data: txData as `0x${string}`,
-			value: 0n
-		});
-		console.log('🚀 ~ file: +page.svelte:48 ~ tx:', tx);
+		// const withPaymaster = state.signer.withPaymasterMiddleware(
+		// 	alchemyPaymasterAndDataMiddleware({
+		// 		provider: state.provider.rpcClient,
+		// 		policyId: GAS_POLICY_ID,
+		// 		entryPoint: BASE_ENTRYPOINT_ADDRESS
+		// 	})
+		// );
+		// // mint nft
+		// const smartAddress = await withPaymaster.getAddress();
+		// const target = NFT_ADDRESS;
+		// const txData = DemoNFT__factory.createInterface().encodeFunctionData('mintEnergy', [
+		// 	target,
+		// 	JSON.stringify(TOKEN_URI)
+		// ]);
+		// const tx = await withPaymaster.sendUserOperation({
+		// 	target: smartAddress as `0x${string}`,
+		// 	data: txData as `0x${string}`,
+		// 	value: 0n
+		// });
+		// console.log('🚀 ~ file: +page.svelte:48 ~ tx:', tx);
 	}
 
 	// mint nft with gas sponsor
 	async function mintNFTWithERC20GasPayment() {
-		if (!state.accountSigner || !state.provider || !state.provider)
-			return console.log('no signer');
-		const smartAddress = await state.accountSigner.getAddress();
+		if (!state.signer || !state.provider || !state.provider) return console.log('no signer');
+		const smartAddress = await state.signer.getAddress();
 		const target = NFT_ADDRESS;
 		const txData = DemoNFT__factory.createInterface().encodeFunctionData('mintEnergy', [
 			target,
 			JSON.stringify(TOKEN_URI)
 		]);
-		await state.accountSigner.sendUserOperation({
+		await state.signer.sendUserOperation({
 			target: smartAddress as `0x${string}`,
 			data: txData as `0x${string}`,
 			value: 0n
@@ -79,7 +83,9 @@
 	</Container>
 	<Container>
 		<section>
-			<button class="button-primary" on:click={mintNFTWithGasSponsor}>Mint NFT With Gas Sponsor</button>
+			<button class="button-primary" on:click={mintNFTWithGasSponsor}
+				>Mint NFT With Gas Sponsor</button
+			>
 		</section>
 	</Container>
 	<Container>
